663da5c3e0956722b73d89789aa34466934dcb62
Сергей Страшнов <life.rus@gmail.com>
2020-03-08 17:48:03 +0600

    events

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,35 @@
 <script>
 	export let name;
+	function changeNameHandler(){
+		name += "click ";
+	}
+	let pos = {
+		x: 0,
+		y: 0
+	}
+	let inputValue = "";
+	function mouseMoveHandler(event){
+		pos.x=event.x;
+		pos.y=event.y;
+	}
+	function submitHandler(event){
+		console.log("inputValue: ", inputValue);
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p>piu</p>
+	<button on:click={changeNameHandler}>Change name</button>
+	<div class="playground" on:mousemove={mouseMoveHandler}>
+	<h2>X: {pos.x}, Y:{pos.y}</h2>
+	</div>
+	<form on:submit|preventDefault|once ={submitHandler}>
+		<input type="text" on:input={event => (inputValue = event.target.value)}/>
+		<button type="submit">Submit form</button>
+	</form>
 </main>
 
-<style>
+<style> 
 	main {
 		text-align: center;
 		padding: 1em;
@@ -27,5 +48,12 @@
 		main {
 			max-width: none;
 		}
+	}
+	.playground {
+		width: 400px;
+		height: 200px;
+		padding: 1rem;
+		margin-bottom: 1rem;
+		border: 1px solid black;
 	}
 </style>
